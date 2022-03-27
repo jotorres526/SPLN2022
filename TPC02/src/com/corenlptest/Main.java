22508729370732326233b5cc1dd859b8388b8bea
jotorres526 <jotorres526@gmail.com>
2022-03-27 21:23:48 +0100

    Stuff

--- TPC02/src/com/corenlptest/Main.java
+++ TPC02/src/com/corenlptest/Main.java
@@ -1,17 +1,25 @@
 package com.corenlptest;
 import edu.stanford.nlp.simple.*;
+import edu.stanford.nlp.trees.Tree;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
-        Document doc = new Document("add your text here! It can contain multiple sentences.");
-        for (Sentence sent : doc.sentences()) {  // Will iterate over two sentences
-            // We're only asking for words -- no need to load any models yet
-            System.out.println("The second word of the sentence '" + sent + "' is " + sent.word(1));
-            // When we ask for the lemma, it will load and run the part of speech tagger
-            System.out.println("The third lemma of the sentence '" + sent + "' is " + sent.lemma(2));
-            // When we ask for the parse, it will load and run the parser
-            System.out.println("The parse of the sentence '" + sent + "' is " + sent.parse());
+        String content = "";
+        try {
+            content = Files.readString(Paths.get("hp.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Document doc = new Document(content);
+        Sentence sent = doc.sentence(0);
+        System.out.println("Sentence: " + sent);
+        for (Tree subtree : sent.parse().subTrees()) {
+            System.out.println("Subtree: " + subtree);
         }
     }
 }
