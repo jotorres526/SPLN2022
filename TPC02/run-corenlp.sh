66b2164773132d3cae56581a0249fb511009ffd1
jotorres526 <jotorres526@gmail.com>
2022-03-27 19:27:50 +0100

    Update

--- TPC02/run-corenlp.sh
+++ TPC02/run-corenlp.sh
@@ -1,3 +1,4 @@
 #/bin/env bash
-java -mx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -outputFormat json -file input.txt
-java -mx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -file input.txt
+#java -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -outputFormat json -file input.txt
+#java -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -file input.txt
+java -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos -file input.txt
